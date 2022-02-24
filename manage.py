e9981723d2091da58e3d9e8c136a0f2b037d1a3b
layersony <layersony@gmail.com>
2022-02-25 01:21:13 +0300

    ft: index view add

--- manage.py
+++ manage.py
@@ -1,5 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
 from flask_script import Manager, Server
+app = Flask(__name__)
+
+# view function
+@app.route('/')
+def index():
+  return render_template('index.html')
 
 # Server
 manager = Manager(app)
